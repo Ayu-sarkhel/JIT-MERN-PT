fc53bab1c11c340b6431e1efc34775c43adda773
Ayu-sarkhel <ayushisarkhel@gmail.com>
2026-09-11 16:19:22 +0530

    Creation of TaskDetails.jsx, uplifted the data from Dashboard.jsx

--- Student-task-management-portal/src/App.jsx
+++ Student-task-management-portal/src/App.jsx
@@ -2,13 +2,41 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Welcome from "./components/Welcome";
 import Dashboard from "./components/Dashboard";
+import {Routes, Route} from "react-router-dom";
+import TaskDetails from "./components/TaskDetails";
+import Tasks from "./components/Tasks";
 
 function App(){
+
+  const[tasks, setTasks] = useState([
+    {
+        id:1,
+        title:"Learn React", 
+        desc:"Understanding Components", 
+        status:"Completed"
+    },
+    {
+        id:2,
+        title:"Learn Javascript", 
+        desc:"Understanding Variables and Functions", 
+        status:"Pending"
+    },
+    {
+        id:3,
+        title:"Learn MongoDB", 
+        desc:"Understanding Databases", 
+        status:"Completed"
+    }
+  ]);
+
   return (
     <div>
       <Navbar />
-      <Welcome />
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard tasks={tasks} setTasks={setTasks} />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/tasks/:id" element={<TaskDetails />} />
+      </Routes>
     </div>
   );
 }
--- Student-task-management-portal/src/components/Dashboard.jsx
+++ Student-task-management-portal/src/components/Dashboard.jsx
@@ -1,33 +1,12 @@
 import StatCard from "./StatCard";
 import TaskCard from "./TaskCard";
 import AddTask from "./AddTask";
-import { useState } from "react";
 
-function Dashboard() {
-    const[tasks, setTasks] = useState([
-        {
-            id:1,
-            title:"Learn React", 
-            desc:"Understanding Components", 
-            status:"Completed"
-        },
-        {
-            id:2,
-            title:"Learn Javascript", 
-            desc:"Understanding Variables and Functions", 
-            status:"Pending"
-        },
-        {
-            id:3,
-            title:"Learn MongoDB", 
-            desc:"Understanding Databases", 
-            status:"Completed"
-        }
-    ]);
-
+function Dashboard(props) {
+    
     function toggleTask(id){
-        setTasks(
-            tasks.map((task)=>{
+        props.setTasks(
+            props.tasks.map((task)=>{
                 if(task.id === id)
                     return {...task, 
                         status: task.status === "Completed" ? "Pending" : "Completed"
@@ -38,7 +17,13 @@ function Dashboard() {
     }
 
     function addTask(newTask){
-        setTasks([...tasks, newTask]);
+        props.setTasks([...props.tasks, newTask]);
+    }
+
+    function deleteTask(id){
+        props.setTasks(
+            props.tasks.filter((task)=>task.id!==id)
+        );
     }
 
     return (
@@ -56,13 +41,15 @@ function Dashboard() {
 
             <div className="tasks-container">
 
-                {tasks.map((task)=>(
+                {props.tasks.map((task)=>(
                     <TaskCard 
-                        key={task.id} 
+                        key={task.id}
+                        id={task.id} 
                         title={task.title} 
                         desc={task.desc} 
                         status={task.status}
                         onToggle={()=>toggleTask(task.id)} 
+                        onDelete={()=>deleteTask(task.id)}
                     />
                 ))}
 
