63a92d3763700e446d1d82c604f4a4f027358fbc
Ayu-sarkhel <ayushisarkhel@gmail.com>
2026-09-15 16:28:55 +0530

    Connected the tasks from backend to frontend

--- Student-task-management-portal/Backend/Server.js
+++ Student-task-management-portal/Backend/Server.js
@@ -9,6 +9,7 @@ const app = express();
 
 // use cors middleware to handle requests
 app.use(cors());
+app.use(express.json());
 
 const tasks = [
     {
@@ -27,6 +28,21 @@ const tasks = [
 
 app.get("/api/tasks", (req,res)=>{
     res.json(tasks);
+})
+
+app.get("/api/tasks/:id", (req,res)=>{
+    const id = Number(req.params.id);
+    const task = tasks.find((task)=> task.id === id);
+    if(!task){
+        return res.status(404).json({message: "Task not found!"});
+    }
+    res.json(task);
+})
+
+app.post("/api/tasks", (req,res)=>{
+    const newTask = req.body;
+    tasks.push(newTask);
+    res.status(201).json(newTask);
 })
 
 // API Route (Testing Backend)
--- Student-task-management-portal/src/components/AddTask.jsx
+++ Student-task-management-portal/src/components/AddTask.jsx
@@ -1,48 +1,59 @@
 import { useState } from "react";
 
-function AddTask(props){
-    const [title, setTitle]=useState("");
-    const [desc, setDesc]=useState("");
+function AddTask(props) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
 
-    function handleSubmit(e){
-        e.preventDefault()
-        const newTask={
-            id:Date.now(),
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const newTask = {
+            id: Date.now(),
             title: title,
-            desc: desc,
+            description: description, 
             status: "Pending"
+        };
+        
+        try{
+            const response = await fetch("http://localhost:5000/api/tasks", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(newTask)
+            });
+    
+            const data = await response.json();
+            props.onAddTask(data);
         }
-        console.log("Object:", newTask)
-        props.onAddTask(newTask);
+
+        catch(error){
+            console.log(error);
+        }
+
     }
 
     return (
         <div>
-             
             <h2>Add Task</h2>
-
             <form onSubmit={handleSubmit}> 
-            
                 <label>Add Title: </label>
-                <input type="text" 
+                <input 
+                    type="text" 
                     value={title}
-                    onChange={(e)=>setTitle(e.target.value)} 
+                    onChange={(e) => setTitle(e.target.value)} 
                 />
 
                 <br /><br />
 
                 <label>Add Description: </label>
-                <input type="text"
-                    value={desc}
-                    onChange={(e)=>setDesc(e.target.value)}
+                <input 
+                    type="text"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                 />
 
                 <br /><br />
 
                 <button type="submit">Add Task</button>
-                
             </form>
-
         </div>
     );
 }
